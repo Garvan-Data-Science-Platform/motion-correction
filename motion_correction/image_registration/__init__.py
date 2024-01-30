1686ee3e85d7afe531602a8737c73a27cc5cfdfb
Tim Kallady <t.kallady@garvan.org.au>
2024-01-30 14:12:42 +1100

    remove pytest dependency

--- motion_correction/image_registration/__init__.py
+++ motion_correction/image_registration/__init__.py
@@ -8,12 +8,15 @@ This is an Astropy affiliated package.
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
 from ._astropy_init import *
+
 # ----------------------------------------------------------------------------
 
 # For egg_info test builds to pass, put package imports here.
-from .cross_correlation_shifts import cross_correlation_shifts, cross_correlation_shifts_FITS
+from .cross_correlation_shifts import (
+    cross_correlation_shifts,
+    cross_correlation_shifts_FITS,
+)
 from .chi2_shifts import chi2_shift, chi2n_map, chi2_shift_iterzoom
 from .register_images import register_images
 from . import fft_tools
-from . import tests
 from . import FITS_tools
